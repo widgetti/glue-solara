034536e6e805f51422469d46bf41993c1abdb842
Iisakki Rotko <iisakki.rotko@gmail.com>
2023-11-03 16:07:03 +0100

    fix: text when no data is selected and display of links

--- glue_solara/linker.py
+++ glue_solara/linker.py
@@ -31,6 +31,10 @@ def add_link(data_collection, data1, attribute1, data2, attribute2):
     data_collection.add_link(link)
 
 
+def stringify_links(link):
+    return str(link._cid1) + " <-> " + str(link._cid2)
+
+
 @solara.component
 def Linker(app: JupyterApplication, show_list: bool = True):
     selected_data1 = solara.use_reactive(0)
@@ -38,10 +42,10 @@ def Linker(app: JupyterApplication, show_list: bool = True):
     selected_data2 = solara.use_reactive(1)
     selected_row2 = solara.use_reactive(0)
     data_collection = app.data_collection
-    # dc_links = solara.use_reactive(data_collection.links)
-    # use_glue_watch(data_collection.hub, glue.core.message)
-    # use_glue_watch(app.session.hub, glue.core.message.Message)
     use_glue_watch(app.session.hub, glue.core.message.Message)
+
+    if len(data_collection.data) == 0:
+        return solara.Text("No data loaded")
 
     def _add_link():
         add_link(
@@ -51,23 +55,13 @@ def Linker(app: JupyterApplication, show_list: bool = True):
             data_collection[selected_data2.value],
             data_collection[selected_data2.value].components[selected_row2.value],
         )
-        # dc_links.set(
-        #     [
-        #         *data_collection.links,
-        #         str(data_collection[selected_data1.value].components[selected_row1.value])
-        #         + "<->"
-        #         + str(data_collection[selected_data2.value].components[selected_row1.value]),
-        #     ]
-        # )
 
-    # with solara.Card("Link Editor", style="width: fit-content;"):
     with solara.Column():
         with solara.Row():
             data_dict = [
                 {"label": data.label, "value": index} for index, data in enumerate(data_collection)
             ]
             with solara.Column():
-                # solara.v.Spacer()
                 with solara.Row(style={"align-items": "start"}):
                     if len(data_collection) > 1:
                         LinkSelector(data_collection, data_dict, selected_data1, selected_row1)
@@ -81,14 +75,10 @@ def Linker(app: JupyterApplication, show_list: bool = True):
                 with solara.Column():
                     solara.Text("Current Links")
                     with solara.v.List(dense=True):
-                        for link in data_collection.links:
-                            if "pix" in str(link):
-                                continue
+                        for link in data_collection.external_links:
                             with solara.v.ListItem():
                                 with solara.v.ListItemContent():
-                                    solara.v.ListItemTitle(children=[str(link)])
-
-        # with solara.CardActions():
+                                    solara.v.ListItemTitle(children=[stringify_links(link)])
 
 
 @solara.component
@@ -107,9 +97,8 @@ def LinkSelector(
             item_text="label",
             item_value="value",
         )
-        # print(selected_data.value, selected_row.value)
         solara.Text(f"Attributes for {data_dict[selected_data.value]['label']}")
-        with solara.v.List(dense=True):
+        with solara.v.List(dense=True, style_="max-height: 50vh; overflow-y: scroll;"):
             with solara.v.ListItemGroup(
                 v_model=selected_row.value,
                 on_v_model=selected_row.set,
