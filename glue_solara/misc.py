4e1084ae6d1ef2c82a9c12fbfbe6f030269e7b73
Iisakki Rotko <iisakki.rotko@gmail.com>
2024-04-04 13:05:49 +0200

    feat: add support for viewing 3D models via glue-jupyter modelviewer

--- glue_solara/misc.py
+++ glue_solara/misc.py
@@ -7,18 +7,19 @@ from glue.viewers.common.viewer import Viewer
 
 @solara.component
 def ToolBar(viewer: Viewer):
-    solara.Row(
-        children=[
-            viewer.toolbar,
-            solara.v.Spacer(),
-            ipypopout.PopoutButton.element(
-                target=solara.Div(
-                    style_="height: calc(100vh - 2px);",
-                    children=[viewer.figure_widget],
-                ),
-                window_features="popup,width=600,height=600",
+    tools = [
+        viewer.toolbar,
+        solara.v.Spacer(),
+        ipypopout.PopoutButton.element(
+            target=solara.Div(
+                style_="height: calc(100vh - 2px);",
+                children=[viewer.figure_widget],
             ),
-        ],
+            window_features="popup,width=600,height=600",
+        ),
+    ]
+    solara.Row(
+        children=tools,
         margin=2,
         style={"align-items": "center"},
     )
